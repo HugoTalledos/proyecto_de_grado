34987a926183d3e480ba2f75184a4e86efdd02d3
HugoTalledos <hugo.talledos@bpsmart.ai>
2021-07-03 22:53:40 -0500

    refactor code move getExtremities to utils

--- utils/util.py
+++ utils/util.py
@@ -6,10 +6,10 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def createFile(data, path, nameFile):
-    today = (datetime.today()).strftime('%Y-%m-%d-%H%M%S')
-    data.to_csv(r'{}\\{}#D{}.csv'.format(
+	today = (datetime.today()).strftime('%Y-%m-%d-%H%M%S')
+	data.to_csv(r'{}\\{}#D{}.csv'.format(
         path, nameFile, today),index=False)
-    logging.info('Temp average file created')
+	logging.info('Temp average file created')
 
 def createArrayLabels(labels):
 	labelsArray = labels.split('#')
@@ -17,3 +17,14 @@ def createArrayLabels(labels):
 	for label in labelsArray:
 		labels.append(label)
 	return labels
+
+def getExtremities(dataset, labels):
+    expressions = createArrayLabels(labels)
+    arrayExtemitiesLabels = []
+    for label in expressions:
+        columnsLabels = []
+        for column in dataset.columns:
+            if label.lower() in column.lower():
+                columnsLabels.append(column)
+        arrayExtemitiesLabels.append(columnsLabels)
+    return arrayExtemitiesLabels
