b1036c786b3bf5d2743873077c83c9edb8c9b8b0
HugoTalledos <hugo.talledos@bpsmart.ai>
2021-09-21 08:53:02 -0500

    Save data players added

---
+++ save_data/main.py
@@ -0,0 +1,77 @@
+import argparse
+import re
+import pandas as pd
+import logging
+from datetime import datetime
+import os
+import sys
+from player import Player
+from base import Base, engine, Session
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def save_data(playerData, playerId): 
+  Base.metadata.create_all(engine)
+  session = Session()
+  
+  player = Player(playerId,
+                  playerData['peso'],
+                  playerData['aniosE'],
+                  playerData['aI_muñeca'],
+                  playerData['aF_muñeca'],
+                  playerData['std_muñeca'],
+                  playerData['aI_codo'],
+                  playerData['aF_codo'],
+                  playerData['std_codo'],
+                  playerData['aI_hombro'],
+                  playerData['aF_hombro'],
+                  playerData['std_hombro'],
+                  playerData['aI_cadera'],
+                  playerData['aF_cadera'],
+                  playerData['std_cadera'],
+                  playerData['aI_rodilla'],
+                  playerData['aF_rodilla'],
+                  playerData['std_rodilla'],
+                  playerData['aI_tobillo'],
+                  playerData['aF_tobillo'],
+                  playerData['std_tobillo'],
+                  playerData['efectividad'],
+                  )
+  session.add(player)
+
+  session.commit()
+  session.close()
+
+def add_data(fileRoot, fileName):
+  root = '{}/compare/temp'.format(os.getcwd())
+  files = os.listdir(root)
+  playerId = ''
+  playerData = {}
+  for file in files:
+    rootFile = '{}/{}'.format(root, file)
+    dataset = pd.read_csv(rootFile)
+    playerId = re.search(r'#PI([0-9]*)', file)
+    extremity = re.search(r'#EX(\S*[0-9]*)#', file)
+    playerData['aI_{}'.format(extremity.group(1).lower())] = dataset['ai'][0]
+    playerData['aF_{}'.format(extremity.group(1).lower())] = dataset['af'][0]
+    playerData['std_{}'.format(extremity.group(1).lower())] = dataset['std'][0]
+
+  rootFile = '{}\\{}_{}.csv'.format(fileRoot, fileName, playerId.group(1))
+  extraData = pd.read_csv(rootFile)
+  playerData['aniosE'] = extraData['years'][0]
+  playerData['peso'] = extraData['peso'][0]
+  playerData['efectividad'] = extraData['efectividad'][0]
+  
+  save_data(playerData, playerId.group(1))
+  
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('root',
+                      help='Ruta de archivos /carpeta1/carpeta2',
+                      type=str)
+  parser.add_argument('fileName',
+                      help='',
+                      type=str)
+  args = parser.parse_args()
+  add_data(args.root, args.fileName)
---
+++ save_data/player.py
@@ -0,0 +1,75 @@
+from sqlalchemy import Column, String, Integer, Float
+
+from base import Base
+
+class Player(Base):
+	__tablename__ = 'players'
+
+	id = Column(String, primary_key=True)
+	peso = Column(String)
+	aniosE = Column(Integer)
+	aIMunieca = Column(Float)
+	aFMunieca = Column(Float)
+	stdMunieca = Column(Float)
+	aICodo = Column(Float)
+	aFCodo = Column(Float)
+	stdCodo = Column(Float)
+	aIHombro = Column(Float)
+	aFHombro = Column(Float)
+	stdHombro = Column(Float)
+	aICadera = Column(Float)
+	aFCadera = Column(Float)
+	stdCadera = Column(Float)
+	aIRodilla = Column(Float)
+	aFRodilla = Column(Float)
+	stdRodilla = Column(Float)
+	aITobillo = Column(Float)
+	aFTobillo = Column(Float)
+	stdTobillo = Column(Float)
+	efectividad = Column(Float)
+
+	def __init__(self,
+        id,
+        peso,
+        aniosE,
+        aImunieca,
+        aFMunieca,
+        stdMunieca,
+        aICodo,
+        aFCodo,
+        stdCodo,
+        aIHombro,
+        aFHombro,
+        stdHombro,
+        aICadera,
+        aFCadera,
+        stdCadera,
+        aIRodilla,
+        aFRodilla,
+        stdRodilla,
+        aITobillo,
+        aFTobillo,
+        stdTobillo,
+        efectividad):
+          self.id = id
+          self.peso = peso
+          self.aniosE = aniosE
+          self.aImunieca =  aImunieca
+          self.aFMunieca =  aFMunieca
+          self.stdMunieca = stdMunieca
+          self.aICodo = aICodo
+          self.aFCodo = aFCodo
+          self.stdCodo =  stdCodo
+          self.aIHombro = aIHombro
+          self.aFHombro = aFHombro
+          self.stdHombro =  stdHombro
+          self.aICadera = aICadera
+          self.aFCadera = aFCadera
+          self.stdCadera =  stdCadera
+          self.aIRodilla =  aIRodilla
+          self.aFRodilla =  aFRodilla
+          self.stdRodilla = stdRodilla
+          self.aITobillo =  aITobillo
+          self.aFTobillo =  aFTobillo
+          self.stdTobillo = stdTobillo
+          self.efectivida = efectividad
