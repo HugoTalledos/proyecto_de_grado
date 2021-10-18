2796f22a1a8876c2573caf987fa9a8baacb2cbc5
HugoTalledos <hugo.talledos@bpsmart.ai>
2021-10-18 18:10:15 -0500

    Firebase instance fixed

--- drivers/firebasePy.py
+++ drivers/firebasePy.py
@@ -1,17 +1,25 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from firebase_admin import auth
 
-_firebaseSDK = None
 
-def getInstance():
-  if _firebaseSDK is None:
-    return _startInstance()
-  else:
-    return _firebaseSDK
+class FirestoreApp:
+  _app = None
 
-def _startInstance():
-  _firebaseSDK =  credentials.Certificate('backend-credentials.json')
-  firebase_admin.initialize_app(_firebaseSDK)
-  return firestore.client()
+  def __init__(self):
+    if self._app is None:
+      self._app = self._start_instance()
+      self.firestoreI = firestore.client()
+      self.authI = auth
 
+
+  def _start_instance(self): 
+    service =  credentials.Certificate('backend-credentials.json')
+    return firebase_admin.initialize_app(service)
+
+  def get_firestore_instance(self):
+    return self.firestoreI
+
+  def get_auth_instance(self):
+    return self.authI
